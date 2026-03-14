511f63df7a4e715fd8e8678654925899b3b82312
Motabhai-dp <176063018+Motabhai-dp@users.noreply.github.com>
2026-03-14 18:47:03 +0530

    modified:   install/gg_bot/share/gg_bot/launch/__pycache__/rsp.launch.cpython-310.pyc 	modified:   src/gg_bot/launch/launch_sim.launch.py

--- src/gg_bot/launch/launch_sim.launch.py
+++ src/gg_bot/launch/launch_sim.launch.py
@@ -2,41 +2,58 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
-
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 
 from launch_ros.actions import Node
 
 
-
 def generate_launch_description():
 
+    package_name = 'gg_bot'
 
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
+    sim_mode = LaunchConfiguration('sim_mode')
 
-    package_name='gg_bot' #<--- CHANGE ME
-
-    rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    declare_sim_mode = DeclareLaunchArgument(
+        'sim_mode',
+        default_value='true',
+        description='Run in simulation mode'
     )
 
-    # Include the Gazebo launch file, provided by the gazebo_ros package
+    rsp = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory(package_name),
+                'launch',
+                'rsp.launch.py'
+            )
+        ),
+        launch_arguments={
+            'use_sim_time': 'true'
+        }.items()
+    )
+
     gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-             )
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('gazebo_ros'),
+                'launch',
+                'gazebo.launch.py'
+            )
+        )
+    )
 
-    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        arguments=['-topic', 'robot_description',
-                                   '-entity', 'my_bot'],
-                        output='screen')
-
+    spawn_entity = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=[
+            '-topic', 'robot_description',
+            '-entity', 'my_bot'
+        ],
+        output='screen'
+    )
 
     diff_drive_spawner = Node(
         package="controller_manager",
@@ -50,10 +67,8 @@ def generate_launch_description():
         arguments=["joint_broad"],
     )
 
-
-
-    # Launch them all!
     return LaunchDescription([
+        declare_sim_mode,
         rsp,
         gazebo,
         spawn_entity,
